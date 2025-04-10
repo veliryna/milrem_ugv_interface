d50281aca6b34cb36d70046347a30ef9d5c5a600
Iryna Velychko <76870532+veliryna@users.noreply.github.com>
2025-04-10 10:41:18 +0300

    feat: show initial ugv location

--- ugv_interface/src/components/MapWidget.vue
+++ ugv_interface/src/components/MapWidget.vue
@@ -1,23 +1,22 @@
 <script setup lang="ts">
-import { GoogleMap, AdvancedMarker } from 'vue3-google-map'
+import { GoogleMap, Marker } from 'vue3-google-map'
 import EngineControlButton from './EngineControlButton.vue';
 
 const center = { lat: 59.4050, lng: 24.5630 }
-const markerOptions = { position: center, label: 'L', title: 'LADY LIBERTY' }
-const pinOptions = { background: '#FBBC04' }
+const markerOptions = { position: center, label: 'UGV' }
 </script>
 
 <template>
   <div class="map-wrapper">
     <GoogleMap
-      api-key="AIzaSyBCSNUcXFyjuJKRL_zHT16T96WcAwbEDjI"
+      api-key=""
       style="width: 60vw; height: 90vh;"
       :center="center"
       :zoom="15"
       :fullscreen-control="false"
       :street-view-control="false"
     >
-    <AdvancedMarker :options="markerOptions" :pin-options="pinOptions"/>
+    <Marker :options="markerOptions" />
     </GoogleMap>
     <EngineControlButton />
   </div>
