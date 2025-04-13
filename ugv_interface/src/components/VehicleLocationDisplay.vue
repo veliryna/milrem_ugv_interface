f8a299a13b75bd6aa115aad474aefb217061da00
Iryna Velychko <76870532+veliryna@users.noreply.github.com>
2025-04-13 12:24:31 +0300

    style(location-display): update layout, add ugv label

--- ugv_interface/src/components/VehicleLocationDisplay.vue
+++ ugv_interface/src/components/VehicleLocationDisplay.vue
@@ -7,6 +7,7 @@ defineProps<{
 
 <template>
   <div class="location-display">
+    <p id="ugv-name-display">UGV📍</p>
     <div id="lat-display">{{ lat.toFixed(6) }}</div>
     <div id="lng-display">{{ lng.toFixed(6) }}</div>
   </div>
@@ -17,22 +18,31 @@ defineProps<{
   position: absolute;
   top: 10px;
   right: 100px;
-  width: 90px;
-  height: 55px;
+  width: auto;
+  height: 40px;
   background-color: rgba(77, 77, 77, 0.8);
   border: 2px solid black;
   color: white;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
   justify-content: center;
   border-radius: 10px;
   text-align: center;
+  padding-left: 10px;
+  padding-right: 5px;
 }
 #lat-display{
   font-weight: bold;
+  padding-left: 5px;
+  padding-right: 5px;
 }
 #lng-display{
+  font-weight: bold;
+  padding-left: 5px;
+  padding-right: 5px;
+}
+#ugv-name-display {
   font-weight: bold;
 }
 </style>
